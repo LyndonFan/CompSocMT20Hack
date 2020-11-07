f1c4b19f8c49884344a4c6b16be6eaa7aae5f87f
LyndonFan <lyndon0808@gmail.com>
2020-11-07 22:30:42 +0000

    can play w/ keys

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import pygame as pg
 import time
 
@@ -6,7 +7,7 @@ import time
 majorIndicator = [True, False, True, False, True, True, False, True, False, True, False, True]
 majorText = "CDEFGAB"
 minorText = ["C#/D♭","D#/E♭","F#/G♭","G#/A♭","A#/B♭"]
-noteText = "C Db D Db E F Gb G Ab A Bb B".split(" ")
+noteText = "C Db D Eb E F Gb G Ab A Bb B".split(" ")
 
 def isMajor(x):
     return majorIndicator[((x%12)+12)%12]
@@ -21,59 +22,77 @@ class Application(tk.Frame):
 
     def create_widgets(self):
         pass
-        # self.hi_there = tk.Button(self)
+        # self.hi_there = ttk.Button(self)
         # self.hi_there["text"] = "Hello World\n(click me)"
         # self.hi_there["command"] = self.say_hi
         # self.hi_there.pack(side="top")
 
-        # self.quit = tk.Button(self, text="QUIT", fg="red",
+        # self.quit = ttk.Button(self, text="QUIT", fg="red",
         #                       command=self.master.destroy)
         # self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
     
 
 root = tk.Tk()
-root.geometry("1000x400")
+root.geometry("1000x300")
 app = Application(master=root)
 
 def convToNote(note):
-    return noteText[note%12]+str(note//12)
+    res = noteText[note%12]+str(note//12)
+    print(res)
+    return res
 
 pg.mixer.init()
 pg.init()
 pg.mixer.set_num_channels(50)
 
+channels = [pg.mixer.Channel(i) for i in range(65-36)]
+sounds = [pg.mixer.Sound("Notes/{}.wav".format(convToNote(note))) for note in range(36,65)]
+
 def playNote(note):
     print(convToNote(note))
-    pg.mixer.music.load("Notes/{}.wav".format(convToNote(note)))
-    pg.mixer.music.play()
-    pg.mixer.music.unload()
+    channels[note-36].play(sounds[note-36],maxtime=2000)
     # wave_obj = sa.WaveObject.from_wave_file("Notes/{}.wav".format(convToNote(note)))
     # play_obj = wave_obj.play()
     # time.sleep(1)
     # play_obj.stop()
+
+for c in channels:
+    c.set_volume(0)
+keyBinds = list("cfvgbnjmk,l.q2w3er5t6y7ui9o0p")
 
 notes = {}
 majorNotes = [36,38,40,41,43,45,47,48,50,52,53,55,57,59,60,62,64]
 for i in range(len(majorNotes)):
     n = majorNotes[i]
     # print(n,majorText[i%7])
-    b = tk.Button(text=majorText[i%7],height=10,width=5,bg="white",anchor="s",command=lambda: playNote(majorNotes[i]))
+    b = tk.Button(
+        text=majorText[i%7],height=10,width=5,
+        highlightbackground="white",fg="black",
+        anchor="s",command=lambda z=n: playNote(z)
+    )
+    b.bind(keyBinds[n-36],lambda z=n: playNote(z))
     b.place(x=50+50*i,y=50)
     notes[n] = b
-    del b
 extra = 0
 minorNotes = [37,39,42,44,46,49,51,54,56,58,61,63]
 for i in range(len(minorNotes)):
     n = minorNotes[i]
     # print(n,minorText[i%5])
-    b = tk.Button(text=minorText[i%5],height=5,width=5,bg="white",command=lambda: playNote(n))
+    b = tk.Button(
+        text=minorText[i%5],height=5,width=5,
+        highlightbackground="black",fg="white",
+        command=lambda z=n: playNote(z)
+    )
+    b.bind(keyBinds[n-36],lambda z=n: playNote(z))
     extra += isMajor(n-2)
     # print(extra)
     b.place(x=25+50*(i+extra),y=50)
     notes[n] = b
-    del b
-playNote(36)
+
+for i in range(len(keyBinds)):
+    root.bind(keyBinds[i],lambda inp,z=36+i: playNote(z))
+root.bind("/",lambda inp,z=48: playNote(z))
+
+for c in channels:
+    c.set_volume(1)
 app.mainloop()

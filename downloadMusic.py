1572a0b25ddad6681b7e3b9ee53cfe785b8d90bd
LyndonFan <lyndon0808@gmail.com>
2020-11-08 01:26:51 +0000

    minor updates

--- downloadMusic.py
+++ downloadMusic.py
@@ -2,7 +2,7 @@ from urllib.request import urlretrieve
 
 # http://theremin.music.uiowa.edu/sound%20files/MIS/Piano_Other/piano/Piano.mf.G3.aiff
 noteText = "C Db D Eb E F Gb G Ab A Bb B".split(" ")
-for i in range(27):
-    note = noteText[i%12] + str(3+i//12)
+for i in range(9,9+12*7+1):
+    note = noteText[i%12] + str(i//12)
     print(note)
     urlretrieve("http://theremin.music.uiowa.edu/sound%20files/MIS/Piano_Other/piano/Piano.ff."+str(note)+".aiff","Notes/"+note+".aiff")
